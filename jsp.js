ff788fbf22098dc789977be7b1b5f6096ccb74ec
Charlesciezki <charlesciezki@yahoo.com>
2016-01-04 17:05:41 -0600

    fourth

--- jsp.js
+++ jsp.js
@@ -14,7 +14,8 @@ var hours = prompt("Please enter your hours");
 var wage = prompt("Please enter your wage");
 if (hours > 40 ){
 	
-	totalincome = ((wage * 1.5) * (hours - 40) + (wage * 40));
+	totalincome = ((wage * 1.5) * (hours - 40) + (wage * 40)); 
+	Math.round(totalincome*100)/100;
 }else{
 	totalincome = wage * hours
 	
