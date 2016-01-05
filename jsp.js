111f2ccb0e6934187468d1f6e0a6c14472ae86e3
Charlesciezki <charlesciezki@yahoo.com>
2016-01-04 21:10:38 -0600

    from home

--- jsp.js
+++ jsp.js
@@ -1,25 +1,46 @@
 "use strict";
-var hours;
-var wage;
-var totalincome;
-var person;
-var regtime;
-var overtime;
-regtime = (wage * hours);
-overtime = ((wage * 1.5) * (hours - 40));
-
-totalincome = hours * wage;
-var person = prompt("Please enter your name");
-var hours = prompt("Please enter your hours");
-var wage = prompt("Please enter your wage");
-if (hours > 40 ){
+function otpay(wage,hours,ot){
 	
-	totalincome = ((wage * 1.5) * (hours - 40) + (wage * 40)); 
+	return ((wage * ot) * (hours - 40) + (wage * 40)); 
 	Math.round(totalincome*100)/100;
-}else{
-	totalincome = wage * hours
+
+};
+
+
+
+function regpay(wage,hours){
+		
+	return wage * hours;
 	
 };
 
-console.log(totalincome);
-console.log("hello " + person + " your total income is " + totalincome);
+
+	var person = prompt("Please enter your name");
+	var hours = prompt("Please enter your hours");
+	var wage = prompt("Please enter your wage");
+
+
+
+
+var totalincome = (wage * hours);
+var regtime = (wage * hours);
+var overtime = ((wage * 1.5) * (hours - 40) + (wage * 40));
+var ot = 1.5;
+
+
+
+
+if (hours > 40 ){
+	totalincome = ((wage * ot) * (hours - 40) + (wage * 40)); 
+	Math.round(totalincome*100)/100;
+	console.log("hello " + person + " your total income is " + totalincome)
+}else{
+	totalincome = (wage * hours)
+	console.log("hello " + person + " your total income is " + totalincome)
+};
+
+
+
+
+
+
